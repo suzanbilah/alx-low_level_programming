624f43afc777c762b96fb02e822be9cd07dfb32f
Oluwapelumi Ogunniyi <pelumi.ogunniyi@gmail.com>
2023-08-11 03:37:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-char: 1 byte(s) a;
-int: 4 byte(s) b;
-	long int: 8 byte(s) c;
-	long long int: 8 byte(s) d;
-float: 4 byte(s) f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
