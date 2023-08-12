ce84c8fd7457dd49be1f3bbaf785d5b4df5b1b27
Oluwapelumi Ogunniyi <pelumi.ogunniyi@gmail.com>
2023-08-12 02:51:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
+ * main - Entry
  * Return: 1 
  */
-int main (void)
+int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fput(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
