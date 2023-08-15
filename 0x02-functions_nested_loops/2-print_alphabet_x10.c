2e6a2ec08053c87f595e00a411449d44f06c833a
Oluwapelumi Ogunniyi <pelumi.ogunniyi@gmail.com>
2023-08-15 12:36:05 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		char letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
