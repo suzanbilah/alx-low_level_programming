6e1c629fa7b8e69e8084bd4868f5193c1ac91f8a
Oluwapelumi Ogunniyi <pelumi.ogunniyi@gmail.com>
2023-08-15 15:14:34 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (int i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	print_to_98(10);
+	return (0);
+}
