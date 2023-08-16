1e90b132e8a2961dfb1ddacadc5b95daadca8ed8
Oluwapelumi Ogunniyi <pelumi.ogunniyi@gmail.com>
2023-08-16 09:13:43 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,33 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table
+ * times_table - Prints the 9 times table starting from 0.
  */
 void times_table(void)
 {
-	int row, col, result;
+	int row, column, product;
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (col = 0; col <= 9; col++)
+		for (column = 0; column <= 9; column++)
+	{
+		product = row * column;
+
+		if (column == 0)
+			_putchar(product + '0');
+		else
 		{
-			result = row * col;
+			_putchar(',');
+			_putchar(' ');
 
-			if (col == 0)
-				_putchar(result + '0');
-			else if (result < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
+			if (product >= 10)
+				_putchar(product / 10 + '0');
 			else
-			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
 
-			if (col != 9)
-			{
-				_putchar(' ');
-			}
+			_putchar(product % 10 + '0');
 		}
-
-		_putchar('\n');
+	}
+		_putchar('\n')
 	}
 }
