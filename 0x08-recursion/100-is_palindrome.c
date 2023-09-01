6efe2cdf20dae376ddd5e68c2497ae1e4963e125
Oluwapelumi Ogunniyi <pelumi.ogunniyi@gmail.com>
2023-08-31 17:05:25 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -40,9 +40,9 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-	if (*(s + 1) != *(s + len - 1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);
-	return check_pal(s, i + 1, len - 1);
+	return (check_pal(s, i + 1, len - 1));
 }
