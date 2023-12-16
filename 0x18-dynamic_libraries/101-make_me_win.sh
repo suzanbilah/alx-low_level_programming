896560218c4cdc34003fab271634a0a9171a6d52
suzanbilah <pelumi.ogunniyi@gmail.com>
2023-12-15 18:56:01 -0800

    final changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Legendarytolex/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+cp /bin/ls ./gm
